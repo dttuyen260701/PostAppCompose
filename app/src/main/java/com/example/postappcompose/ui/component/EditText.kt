82059dc6e7125758efb22743da5674fc10282712
Thanh Tuyen <dttuyen260701@gmail.com>
2023-03-20 00:13:14 +0700

    add Compose EditText.kt

---
+++ app/src/main/java/com/example/postappcompose/ui/component/EditText.kt
@@ -0,0 +1,14 @@
+package com.example.postappcompose.ui.component
+
+import android.widget.EditText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun EditText(
+    modifier: Modifier = Modifier,
+    text: String,
+    onTextChange: (String) -> Unit,
+) {
+
+}
