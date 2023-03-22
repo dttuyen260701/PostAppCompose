5efe969189471e9b0acda61fb541ea553086731f
Thanh <dttuyen260701@gmail.com>
2023-03-22 18:09:26 +0700

    Config Theme.kt

--- app/src/main/java/com/example/postappcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/postappcompose/ui/theme/Theme.kt
@@ -1,22 +1,23 @@
 package com.example.postappcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette = PostColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    backgroundGradient = listOf(NavyBlue, NeonBlue, NavyBlue)
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorPalette = PostColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
-
+    secondary = Teal200,
+    backgroundGradient = listOf(NavyBlue, NeonBlue, NavyBlue)
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
@@ -39,9 +40,75 @@ fun PostAppComposeTheme(
     }
 
     MaterialTheme(
-        colors = colors,
+        colors = debugColors(darkTheme),
         typography = Typography,
         shapes = Shapes,
         content = content
     )
 }
+
+@Stable
+class PostColors(
+    primary: Color,
+    primaryVariant: Color,
+    secondary: Color,
+    backgroundGradient: List<Color>,
+) {
+    var primary by mutableStateOf(primary)
+        private set
+    var primaryVariant by mutableStateOf(primaryVariant)
+        private set
+    var secondary by mutableStateOf(secondary)
+        private set
+    var backgroundGradient by mutableStateOf(backgroundGradient)
+        private set
+
+    fun update(other: PostColors) {
+        primary = other.primary
+        primaryVariant = other.primaryVariant
+        secondary = other.secondary
+        backgroundGradient = other.backgroundGradient
+    }
+
+    fun copy(): PostColors = PostColors(
+        primary = primary,
+        primaryVariant = primaryVariant,
+        secondary = secondary,
+        backgroundGradient = backgroundGradient
+    )
+}
+
+@Composable
+fun ProvidePostColors(
+    colors: PostColors,
+    content: @Composable () -> Unit
+) {
+    val colorPalette = remember {
+        colors.copy()
+    }
+    colorPalette.update(colors)
+    CompositionLocalProvider(LocalPostColors provides  colorPalette, content = content)
+}
+
+private val LocalPostColors = staticCompositionLocalOf<PostColors> {
+    error("No LocalPostColors provided")
+}
+
+fun debugColors(
+    darkTheme: Boolean,
+    debugColor: Color = Color.Magenta
+) = Colors(
+    primary = debugColor,
+    primaryVariant = debugColor,
+    secondary = debugColor,
+    secondaryVariant = debugColor,
+    background = debugColor,
+    surface = debugColor,
+    error = debugColor,
+    onPrimary = debugColor,
+    onSecondary = debugColor,
+    onBackground = debugColor,
+    onSurface = debugColor,
+    onError = debugColor,
+    isLight = !darkTheme
+)
